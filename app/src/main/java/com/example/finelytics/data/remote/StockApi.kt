a87dd23aede399a6a1379af94e17286ca35cb8d1
Sumit99089 <sumitlaha47@gmail.com>
2025-07-12 00:08:50 +0530

    Fixed Minor Bugs

--- app/src/main/java/com/example/finelytics/data/remote/StockApi.kt
+++ app/src/main/java/com/example/finelytics/data/remote/StockApi.kt
@@ -8,11 +8,11 @@ import retrofit2.http.GET
 interface StockApi {
     @GET("query?function=LISTING_STATUS")
     suspend fun getListings(
-        @Query("apiKey") apiKey:String = API_KEY
+        @Query("apikey") apiKey:String = API_KEY
     ): ResponseBody
 
     companion object{
-        const val API_KEY = "MW336TNEC86O2TSM"
+        const val API_KEY = "UVO9EA7HR6Y0VPG7"
         const val BASE_URL = "https://alphavantage.co/"
     }
 }
